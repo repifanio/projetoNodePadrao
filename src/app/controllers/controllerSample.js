1b46b6eee8e2a23122c6e82ccfc9347421c4a40f
repifanio <rafael.cepifanio@gmail.com>
2020-03-14 20:22:29 -0300

    Retiradas linhas desnecessárias

--- src/app/controllers/controllerSample.js
+++ src/app/controllers/controllerSample.js
@@ -2,9 +2,6 @@ import ModelSample from '../models/modelSample';
 
 class ControllerSample {
   async store(req, res) {
-    console.log('teste');
-    console.log(req.name);
-
     const modelSample = await ModelSample.create(req.body);
 
     return res.json(modelSample);
